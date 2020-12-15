8ebc83296d2fd7afd9c3b62e7b3fa441dc9bbfd2
Patricio-Vasquez-AIEP <patricio.vasquezmar@correoaiep.cl>
2020-12-14 22:05:40 -0300

    14122020

--- TALLER DE PROGRAMACIÓN/ENE_PRO201_2020/SCRIPT SQL/SCRIPT_TABLAS-DB_ENE_PRO201_2020.sql
+++ TALLER DE PROGRAMACIÓN/ENE_PRO201_2020/SCRIPT SQL/SCRIPT_TABLAS-DB_ENE_PRO201_2020.sql
@@ -16,30 +16,6 @@ insert logins  select 'admin','admin','Administrador'
 
 go
 
-alter proc sp_logins(@usuario varchar(30), @clave varchar(30))
-as begin
 
- if not exists ( select *  from logins where usuario = @usuario)
-	 begin
-		select 1 as CodigoRet,
-			   'Usuario incorrecto' as MensajeRet
-			   return
-	 end
-
- if not exists ( select *  from logins where usuario = @usuario and clave = @clave)
-	 begin
-		select 2 as CodigoRet,
-			   'Clave incorrecta' as MensajeRet
-			   return
-	 end
-
-end
-
-/*
-VALIDAR LOGINS
-select * from logins
-exec sp_logins 'admin','admin'
-
-*/
 
 go
